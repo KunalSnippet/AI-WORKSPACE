506ede9709cc8731dab41f453161b858f20beec9
Kunal <kunalbgr23@gmail.com>
2026-07-05 14:32:00 +0530

    feat: update project structure and dependencies
    - Changed package type from CommonJS to ES Module
    - Added development scripts using nodemon and dotenv
    - Introduced new dependencies: cookie-parser, cors, dotenv, and express
    - Created app.js to set up Express server with CORS and cookie parsing
    - Added constant.js for database name configuration
    - Initialized index.js to load environment variables
    - Added .gitkeep file in public/temp directory to maintain folder structure

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
+app.use(cookieParser());
+
+app.use(express.json({
+  limit: "100mb"
+}));
+
+app.use(express.urlencoded({
+  limit: "100mb"
+}));
+
+app.use(express.static("public"));
+
+export default app;
